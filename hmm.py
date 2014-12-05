71722d7abf86a21b32da6551e9ae8291ece16e7d
thejinxters <brandon.mikulka@colorado.edu>
2014-12-05 14:38:33 -0700

    fix more snakecase

--- hmm.py
+++ hmm.py
@@ -25,26 +25,26 @@ class Hmm(object):
       if sequence[0].input == state:
         state_count += 1
     # divide by the number of total sequences
-    state_probability = state_count / len(self.training_set)
+    state_probability = state_count / float( len(self.training_set) )
     # Natural log that puppy and return it
     return Math.log(state_probability, 2)
 
   # Returns the log probability assigned by this HMM to a
-  # transition from 'fromState' to 'toState'
-  def log_trans_prob(self, fromState, toState):
+  # transition from 'from_state' to 'to_state'
+  def log_trans_prob(self, from_state, to_state):
     transition_from_to_count = 0
     transition_count = 0
     # For all testing sequences
-    # Count the number of transitions between fromState and to State
+    # Count the number of transitions between from_state and to State
     for sequence in self.training_set:
       for i in len(sequence)-1:
-        if sequence[i].input == fromState and sequence[i+1] == tostate:
+        if sequence[i].input == from_state and sequence[i+1] == to_state:
           transition_from_to_count += 1
         if sequence[i].input != sequence[i=1].input
           transition_count += 1
     # Using Laplace smoothing:
-    # Divide by number of transitions from the fromState to any State
-    transition_probability = (transition_from_to_count+1) / (transition_count + self.unique_state_count)
+    # Divide by number of transitions from the from_state to any State
+    transition_probability = (transition_from_to_count+1) / float(transition_count + self.unique_state_count)
     # Natural log that puppy and return it
     return Math.log(transition_probability, 2)
 
@@ -63,6 +63,6 @@ class Hmm(object):
             output_count += 1
     # Using Laplace smoothing:
     # Divide by the number of times the state occurs
-    output_probability = (output_count + 1) / (state_count + self.unique_outputs_count)
+    output_probability = (output_count + 1) / float (state_count + self.unique_outputs_count)
     # Natural log that puppy and return it
     return Math.log(output_probability, 2)
