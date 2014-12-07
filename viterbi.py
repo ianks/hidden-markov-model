8aa7df0fa90d198c2f1d06b1853b3fce060db32d
thejinxters <brandon.mikulka@colorado.edu>
2014-12-06 17:23:58 -0700

    add print statements to show the viterbi is working

--- viterbi.py
+++ viterbi.py
@@ -3,6 +3,7 @@ try:
 except:
     pass
 
+import sys
 import math as Math
 
 class Viterbi(object):
@@ -26,8 +27,11 @@ class Viterbi(object):
             back_pointer[0][state] = self.hmm.start_prob(state) * self.hmm.output_prob(state, output[0])
             path[state] = [state]
 
+        print "Calculating most likely sequence... (one dot for every output)"
         # Run Viterbi for t > 0
         for t in range(1, len(output)):
+            sys.stdout.write('.')
+            sys.stdout.flush()
             back_pointer.append({})
             newpath = {}
 
