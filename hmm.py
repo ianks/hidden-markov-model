b69a3391c257e063e12d6a909284ccb81c0f2a3d
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-12-07 00:07:29 +0000

    Cache the proabilities

--- hmm.py
+++ hmm.py
@@ -15,6 +15,8 @@ class Hmm(object):
         self.unique_outputs_count = data.unique_outputs_count
         self.training_set = data.training.sequences
         self.states = data.statekeys
+        self.transitions_probabilities = {}
+        self.output_probabilities = {}
 
     # Returns the log probability assigned by this HMM to a
     # transition from the dummy start state to a given state
@@ -29,9 +31,20 @@ class Hmm(object):
         # Natural log that puppy and return it
         return state_probability
 
+    # Cache the trans probabilities
+    def trans_prob(self, from_state, to_state):
+        if from_state in self.transitions_probabilities:
+            if to_state in self.transitions_probabilities[from_state]:
+                return self.transitions_probabilities[from_state][to_state]
+
+        prob = self._trans_prob(from_state, to_state)
+        self.transitions_probabilities[from_state] = { to_state : prob }
+
+        return prob
+
     # Returns the log probability assigned by this HMM to a
     # transition from 'from_state' to 'to_state'
-    def trans_prob(self, from_state, to_state):
+    def _trans_prob(self, from_state, to_state):
         transition_from_to_count = 0
         transition_count = 0
         # For all testing sequences
@@ -48,9 +61,20 @@ class Hmm(object):
         # Natural log that puppy and return it
         return transition_probability
 
+    # Cache output probabilities
+    def output_prob(self, state, output):
+        if state in self.output_probabilities:
+            if output in self.output_probabilities[state]:
+                return self.output_probabilities[state][output]
+
+        prob = self._output_prob(state, output)
+        self.output_probabilities[state] = { output : prob }
+
+        return prob
+
     # Returns the log probability of 'state' emitting
     # 'output'
-    def output_prob(self, state, output):
+    def _output_prob(self, state, output):
         output_count = 0
         state_count = 0
         # For all testing sequences
